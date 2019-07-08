c88562457b62dbe2075ee1f90eabaa9ada069898
Plaban Majumder <plaban@PM-IM.local>
2019-07-08 16:27:17 +0100

    initial commit

---
+++ src/test/java/selenium/MakeMyTrip.java
@@ -0,0 +1,27 @@
+package selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MakeMyTrip {
+	
+public WebDriver driver;
+	
+	@Before
+	public void setUP() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://www.makemytrip.com/");
+	}
+	
+	@After
+	public void tearDown() {
+		//driver.quit();
+		
+	}
+
+}
